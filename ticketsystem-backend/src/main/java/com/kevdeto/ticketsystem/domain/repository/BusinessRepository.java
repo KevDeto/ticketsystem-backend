9701889303045a49f41347177e59f1fafedb46f2
KevDeto <contactohuayta@gmail.com>
2025-08-07 18:23:29 -0300

    termine de implementar todos los repository de las entidades

--- ticketsystem-backend/src/main/java/com/kevdeto/ticketsystem/domain/repository/BusinessRepository.java
+++ ticketsystem-backend/src/main/java/com/kevdeto/ticketsystem/domain/repository/BusinessRepository.java
@@ -1,9 +1,11 @@
 package com.kevdeto.ticketsystem.domain.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.kevdeto.ticketsystem.domain.entity.BusinessEntity;
 
+@Repository
 public interface BusinessRepository extends JpaRepository<BusinessEntity, Long> {
 
 }
