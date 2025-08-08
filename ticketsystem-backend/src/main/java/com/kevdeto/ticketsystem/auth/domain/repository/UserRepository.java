dd435c6bf8626096704ae444912c76d199ef92b6
KevDeto <contactohuayta@gmail.com>
2025-08-08 16:41:32 -0300

    agregue dos metodos nuevos para trabajar con el email y username del usuario

--- ticketsystem-backend/src/main/java/com/kevdeto/ticketsystem/auth/domain/repository/UserRepository.java
+++ ticketsystem-backend/src/main/java/com/kevdeto/ticketsystem/auth/domain/repository/UserRepository.java
@@ -1,11 +1,14 @@
 package com.kevdeto.ticketsystem.auth.domain.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import com.kevdeto.ticketsystem.auth.domain.UserEntity;
+import com.kevdeto.ticketsystem.auth.domain.model.UserEntity;
 
 @Repository
 public interface UserRepository extends JpaRepository<UserEntity, Long> {
-
+	Optional<UserEntity> findByEmail(String email);
+	boolean existsByUsername(String username);
 }
