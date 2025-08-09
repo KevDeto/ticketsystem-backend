941e398fe051a217e39ba6cd3db7a846e9510857
KevDeto <contactohuayta@gmail.com>
2025-08-09 13:48:23 -0300

    movi la clase UserEntity a un paquete model y tambien cree la clase UserDetails para trabajar con el UserEntity

---
+++ ticketsystem-backend/src/main/java/com/kevdeto/ticketsystem/auth/domain/model/UserEntity.java
@@ -0,0 +1,39 @@
+package com.kevdeto.ticketsystem.auth.domain.model;
+
+import com.kevdeto.ticketsystem.auth.domain.enums.UserRole;
+import com.kevdeto.ticketsystem.domain.entity.BusinessEntity;
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToOne;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@Entity
+@Table(name = "users")
+public class UserEntity {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+	private String username;
+	private String email;
+	private String password;
+	@Enumerated(EnumType.STRING)
+	private UserRole role;
+
+	@OneToOne(mappedBy = "user", cascade = CascadeType.ALL)
+	private BusinessEntity business;
+}
