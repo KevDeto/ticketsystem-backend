b462b50e4bfe122ef128849bee165b6028c9a89d
KevDeto <contactohuayta@gmail.com>
2025-08-24 18:30:56 -0300

    se modifico la seguridad

--- ticketsystem-backend/src/main/java/com/kevdeto/ticketsystem/auth/security/config/SecurityConfig.java
+++ ticketsystem-backend/src/main/java/com/kevdeto/ticketsystem/auth/security/config/SecurityConfig.java
@@ -1,5 +1,5 @@
 package com.kevdeto.ticketsystem.auth.security.config;
-
+import com.kevdeto.ticketsystem.config.CorsConfig;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
@@ -14,6 +14,7 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfigurationSource;
 
 import com.kevdeto.ticketsystem.auth.application.service.UserDetailsServiceImpl;
 import com.kevdeto.ticketsystem.auth.security.jwt.JwtAuthenticationFilter;
@@ -22,32 +23,34 @@ import com.kevdeto.ticketsystem.auth.security.jwt.JwtAuthenticationFilter;
 @EnableWebSecurity
 @EnableMethodSecurity
 public class SecurityConfig {
+
+    private final CorsConfigurationSource corsConfigurationSource;
 	
     private final JwtAuthenticationFilter jwtAuthenticationFilter;
     private final UserDetailsServiceImpl userDetailsService;
 
     public SecurityConfig(JwtAuthenticationFilter jwtAuthenticationFilter,
-                          UserDetailsServiceImpl userDetailsService) {
+                          UserDetailsServiceImpl userDetailsService, CorsConfigurationSource corsConfigurationSource) {
         this.jwtAuthenticationFilter = jwtAuthenticationFilter;
         this.userDetailsService = userDetailsService;
+        this.corsConfigurationSource = corsConfigurationSource;
     }
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         return http
+        		.cors(cors -> cors.configurationSource(corsConfigurationSource))
                 .csrf(AbstractHttpConfigurer::disable)
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers(
-                                "/api/auth/**",  // Ruta de autenticación
+//                                "/api/auth/**",  // Ruta de autenticación
                                 "/v3/api-docs/**",  // Documentación OpenAPI JSON
                                 "/swagger-ui/**",  // Interfaz Swagger UI
                                 "/swagger-ui.html"  // Página HTML de Swagger
                             ).permitAll()
-                        .requestMatchers("/auth/**").permitAll()
-                        .requestMatchers(HttpMethod.GET, "/products/**").permitAll()
-                        .requestMatchers("/users/**").hasRole("USER")
-                        .requestMatchers("/admin/**").hasRole("ADMIN")
+                        .requestMatchers("/api/auth/**").permitAll()
+                        .requestMatchers(HttpMethod.GET, "/api/products/**").permitAll()
                         .anyRequest().authenticated()
                 )
                 .userDetailsService(userDetailsService)
