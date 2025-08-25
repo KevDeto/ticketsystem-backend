726f01f97ae3b9329377f1f4a42d54cb598ee475
KevDeto <contactohuayta@gmail.com>
2025-08-25 19:13:09 -0300

    se comento los permisos CORS ya que la aplicacion sera para un entorno local, pero en algun futuro se busca que tenga una base de datos central en la nube

--- ticketsystem-backend/src/main/java/com/kevdeto/ticketsystem/config/CorsConfig.java
+++ ticketsystem-backend/src/main/java/com/kevdeto/ticketsystem/config/CorsConfig.java
@@ -8,31 +8,31 @@ import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
-@Configuration
+//@Configuration
 public class CorsConfig {
-
-    @Bean
-    public CorsConfigurationSource corsConfigurationSource() {
-        CorsConfiguration configuration = new CorsConfiguration();
-
-        // 🌍 Orígenes permitidos (frontend)
-        configuration.setAllowedOrigins(List.of("http://localhost:3000")); // Dev
-        // En prod: List.of("https://mi-frontend.com")
-
-        // Métodos permitidos
-        configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
-
-        // Headers permitidos
-        configuration.setAllowedHeaders(List.of("Authorization", "Content-Type"));
-
-        // Cookies habilitadas
-        configuration.setAllowCredentials(true);
-
-        // Tiempo de cacheo de la config (opcional)
-        configuration.setMaxAge(3600L);
-
-        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-        source.registerCorsConfiguration("/**", configuration);
-        return source;
-    }
+//
+//    @Bean
+//    public CorsConfigurationSource corsConfigurationSource() {
+//        CorsConfiguration configuration = new CorsConfiguration();
+//
+//        // 🌍 Orígenes permitidos (frontend)
+//        configuration.setAllowedOrigins(List.of("http://localhost:3000")); // Dev
+//        // En prod: List.of("https://mi-frontend.com")
+//
+//        // Métodos permitidos
+//        configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
+//
+//        // Headers permitidos
+//        configuration.setAllowedHeaders(List.of("Authorization", "Content-Type"));
+//
+//        // Cookies habilitadas
+//        configuration.setAllowCredentials(true);
+//
+//        // Tiempo de cacheo de la config (opcional)
+//        configuration.setMaxAge(3600L);
+//
+//        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+//        source.registerCorsConfiguration("/**", configuration);
+//        return source;
+//    }
 }
